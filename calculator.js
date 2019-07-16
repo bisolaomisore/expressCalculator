1b4a511f0776d9e70e47360b4e9cad201850c7b0
Sola <solaocodes@gmai.com>
2019-07-15 20:40:54 -0400

    A travis fail.

--- calculator.js
+++ calculator.js
@@ -3,7 +3,6 @@ var express = require("express");
 var app = express();
 var PORT = 8080;
 
-// this is a get route.
 app.get('/:operator/:num1/:num2', function(req,res){
 
 	var operator = req.params.operator
